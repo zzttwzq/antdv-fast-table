11e5077d4061da3b8ccf0fad50d30fd8b1af84ec
wuzhiqiang <1076976262@qq.com>
2022-03-17 14:41:12 +0800

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -2,13 +2,13 @@
 # npm adduser
 
 # 当前账号信息
-# npm who am i
+npm who am i
 
 # # 发布
 # npm publish
-# if [ $? != 0 ]; then
-#     exit 0
-# fi
+if [ $? != 0 ]; then
+    exit 0
+fi
 
 # # git 添加到暂存区
 git add .
@@ -19,5 +19,4 @@ read -p "请输入提交内容：" input
 echo "feat()：<$v> $input"
 git commit -m "feat()：<$v> $input"
 
-# git 推送
 git push
